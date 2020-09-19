935ee75a6b73b3162c3586e9f3baade92028aa91
Arthur Pastel <arthur.pastel@gmail.com>
2020-09-19 16:53:39 +0200

    More tests for model definition

---
+++ tests/unit/test_model_definition.py
@@ -0,0 +1,272 @@
+from typing import Any, Optional
+
+import pytest
+from pydantic import Field as PDField
+from pydantic.error_wrappers import ValidationError
+
+from odmantic.fields import Field
+from odmantic.model import EmbeddedModel, Model
+from odmantic.reference import Reference
+
+
+class TheClassName(Model):
+    ...
+
+
+class TheClassNameModel(Model):
+    ...
+
+
+class TheClassNameOverriden(Model):
+    __collection__ = "collection_name"
+
+
+def test_auto_collection_name():
+
+    assert TheClassName.__collection__ == "the_class_name"
+
+    assert TheClassNameModel.__collection__ == "the_class_name"
+
+    assert TheClassNameOverriden.__collection__ == "collection_name"
+
+
+def test_auto_collection_name_nested():
+    class theNestedClassName(Model):
+        ...
+
+    assert theNestedClassName.__collection__ == "the_nested_class_name"
+
+    class TheNestedClassNameOverriden(Model):
+        __collection__ = "collection_name"
+
+    assert TheNestedClassNameOverriden.__collection__ == "collection_name"
+
+
+def test_duplicated_key_name():
+    with pytest.raises(TypeError):
+
+        class M(Model):
+            a: int
+            b: int = Field(key_name="a")
+
+
+def test_duplicated_key_name_in_reference():
+    class Referenced(Model):
+        a: int
+
+    with pytest.raises(TypeError):
+
+        class Base(Model):
+            a: int = Field(key_name="referenced")
+            referenced: Referenced = Reference()
+
+
+def test_duplicate_key_name_definition():
+    with pytest.raises(TypeError):
+
+        class Base(Model):
+            a: int = Field(key_name="referenced")
+            b: int = Field(key_name="referenced")
+
+
+def test_key_name_containing_dollar_sign():
+    class Base(Model):
+        a: int = Field(key_name="a$b")
+
+
+def test_key_starting_with_dollar_sign():
+    with pytest.raises(TypeError):
+
+        class Base(Model):
+            a: int = Field(key_name="$a")
+
+
+def test_key_containing_dot():
+    with pytest.raises(TypeError):
+
+        class Base(Model):
+            b: int = Field(key_name="a.b")
+
+
+def test_wrong_model_field():
+    with pytest.raises(TypeError, match="use odmantic.Field instead of pydantic.Field"):
+
+        class M(Model):
+            a: int = PDField()
+
+
+def test_unknown_model_field():
+    class UnknownType:
+        pass
+
+    def U() -> Any:
+        return UnknownType()
+
+    with pytest.raises(TypeError):
+
+        class M(Model):
+            a: int = U()
+
+
+def test_model_default_simple():
+    class M(Model):
+        f: int = 3
+
+    instance = M()
+    assert instance.f == 3
+
+
+def test_model_default_with_field():
+    class M(Model):
+        f: int = Field(default=3)
+
+    instance = M()
+    assert instance.f == 3
+
+
+def test_optional_field():
+    class M(Model):
+        f: Optional[str]
+
+    assert M(f=None).f is None
+    assert M(f="hello world").f == "hello world"
+
+
+def test_optional_field_with_default():
+    class M(Model):
+        f: Optional[str] = None
+
+    assert M().f is None
+    assert M(f="hello world").f == "hello world"
+
+
+def test_field_with_invalid_default_type():
+    with pytest.raises(TypeError, match="Unhandled field definition"):
+
+        class M(Model):
+            f: Optional[int] = "a"  # type: ignore
+
+
+@pytest.mark.skip("Wait for feedback on an pydantic issue")
+def test_field_with_invalid_default_type_in_field():
+    with pytest.raises(TypeError, match="Unhandled field definition"):
+
+        class M(Model):
+            f: Optional[int] = Field("a")
+
+
+@pytest.mark.skip("Wait for feedback on an pydantic issue")
+def test_field_with_invalid_default_value_in_field_at_definition():
+    with pytest.raises(TypeError, match="Unhandled field definition"):
+
+        class M(Model):
+            f: Optional[int] = Field(3, gt=5)
+
+
+def test_field_with_invalid_default_value_in_field_at_instantiation():
+    class M(Model):
+        f: Optional[int] = Field(3, gt=5)
+
+    with pytest.raises(ValidationError):
+        M()
+
+
+def test_optional_field_with_field_settings():
+    class M(Model):
+        f: Optional[str] = Field("hello world", key_name="my_field")
+
+    assert M().f == "hello world"
+    assert M(f=None).f is None
+
+
+def test_unable_to_generate_primary_field():
+    with pytest.raises(TypeError, match="can't automatically generate a primary field"):
+
+        class A(Model):
+            id: str
+
+
+def test_define_alternate_primary_key():
+    class M(Model):
+        name: str = Field(primary_field=True)
+
+    instance = M(name="Jack")
+    assert instance.doc() == {"_id": "Jack"}
+
+
+def test_weird_overload_id_field():
+    class M(Model):
+        id: int
+        name: str = Field(primary_field=True)
+
+    instance = M(id=15, name="Johnny")
+    assert instance.doc() == {"_id": "Johnny", "id": 15}
+
+
+@pytest.mark.skip("Not implemented")
+def test_overload_id_with_another_primary_key():
+    with pytest.raises(TypeError, match="cannot define multiple primary keys"):
+
+        class M(Model):
+            id: int
+            number: int = Field(primary_key=True)
+
+
+def test_untyped_field_definition():
+    with pytest.raises(TypeError, match="defined without type annotation"):
+
+        class M(Model):
+            a = 3
+
+
+def test_multiple_primary_key():
+    with pytest.raises(TypeError, match="Duplicated key_name"):
+
+        class M(Model):
+            a: int = Field(primary_field=True)
+            b: int = Field(primary_field=True)
+
+
+def test_model_with_implicit_reference_error():
+    class A(Model):
+        pass
+
+    with pytest.raises(TypeError, match="without a Reference assigned"):
+
+        class B(Model):
+            a: A
+
+
+def test_embedded_model_with_primary_key():
+    with pytest.raises(TypeError, match="cannot define a primary field"):
+
+        class A(EmbeddedModel):
+            f: int = Field(primary_field=True)
+
+
+def test_invalid_collection_name_dollar():
+    with pytest.raises(TypeError, match=r"cannot contain '\$'"):
+
+        class A(Model):
+            __collection__ = "hello$world"
+
+
+def test_invalid_collection_name_empty():
+    with pytest.raises(TypeError, match="cannot be empty"):
+
+        class A(Model):
+            __collection__ = ""
+
+
+def test_invalid_collection_name_contain_system_dot():
+    with pytest.raises(TypeError, match="cannot start with 'system.'"):
+
+        class A(Model):
+            __collection__ = "system.hi"
+
+
+def test_untouched_fields():
+    class A(Model):
+        __random_private_thing__: int = 3
+
+    assert "my_prop" not in A.__odm_fields__
---
+++ tests/unit/test_model_logic.py
@@ -0,0 +1,114 @@
+import pytest
+from bson.objectid import ObjectId
+from pydantic.error_wrappers import ValidationError
+
+from odmantic.fields import Field
+from odmantic.model import EmbeddedModel, Model
+from tests.zoo.person import PersonModel
+
+
+def test_repr_model():
+    class M(Model):
+        a: int
+
+    instance = M(a=5)
+    assert repr(instance) == f"M(id={repr(instance.id)}, a=5)"
+
+
+def test_repr_embedded_model():
+    class M(EmbeddedModel):
+        a: int
+
+    instance = M(a=5)
+    assert repr(instance) == "M(a=5)"
+
+
+def test_fields_modified_no_modification():
+    class M(Model):
+        f: int
+
+    instance = M(f=0)
+    assert instance.__fields_modified__ == set(["f", "id"])
+
+
+def test_fields_modified_with_default():
+    class M(Model):
+        f: int = 5
+
+    instance = M(f=0)
+    assert instance.__fields_modified__ == set(["f", "id"])
+
+
+def test_fields_modified_one_update():
+    class M(Model):
+        f: int
+
+    instance = M(f=0)
+    instance.f = 1
+    assert instance.__fields_modified__ == set(["f", "id"])
+
+
+def test_field_update_with_invalid_data_type():
+    class M(Model):
+        f: int
+
+    instance = M(f=0)
+    with pytest.raises(ValidationError):
+        instance.f = "aa"  # type: ignore
+
+
+def test_field_update_with_invalid_data():
+    class M(Model):
+        f: int = Field(gt=0)
+
+    instance = M(f=1)
+    with pytest.raises(ValidationError):
+        instance.f = -1
+
+
+def test_validate_does_not_copy():
+    instance = PersonModel(first_name="Jean", last_name="Pierre")
+    assert PersonModel.validate(instance) is instance
+
+
+def test_validate_from_dict():
+    instance = PersonModel.validate({"first_name": "Jean", "last_name": "Pierre"})
+    assert isinstance(instance, PersonModel)
+    assert instance.first_name == "Jean" and instance.last_name == "Pierre"
+
+
+def test_fields_modified_on_construction():
+    instance = PersonModel(first_name="Jean", last_name="Pierre")
+    assert instance.__fields_modified__ == set(["first_name", "last_name", "id"])
+
+
+def test_fields_modified_on_document_parsing():
+    instance = PersonModel.parse_doc(
+        {"_id": ObjectId(), "first_name": "Jackie", "last_name": "Chan"}
+    )
+    assert instance.__fields_modified__ == set(["first_name", "last_name", "id"])
+
+
+def test_fields_modified_on_object_parsing():
+    instance = PersonModel.parse_obj(
+        {"_id": ObjectId(), "first_name": "Jackie", "last_name": "Chan"}
+    )
+    assert instance.__fields_modified__ == set(["first_name", "last_name", "id"])
+
+
+def test_change_primary_key_value():
+    class M(Model):
+        ...
+
+    instance = M()
+    with pytest.raises(NotImplementedError, match="assigning a new primary key"):
+        instance.id = 12
+
+
+def test_model_copy_not_implemented():
+    class M(Model):
+        ...
+
+    instance = M()
+    with pytest.raises(NotImplementedError):
+        instance.copy()
