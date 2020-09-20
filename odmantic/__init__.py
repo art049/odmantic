d1d1912b33093b4b9d77148210ed0611e1269e92
Arthur Pastel <arthur.pastel@gmail.com>
2020-09-20 20:25:04 +0200

    Unify version storage with poetry

--- odmantic/__init__.py
+++ odmantic/__init__.py
@@ -3,4 +3,11 @@ from .model import EmbeddedModel, Model
 
 __all__ = ["AIOEngine", "Model", "EmbeddedModel"]
 
-__version__ = "0.0.0b0"
+# Cleanest way to handle version changes with poetry while not hardcoding the version
+# https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore
+
+__version__ = importlib_metadata.version(__name__)
