8615ab79de8f4dab02181a15d8ab6a1a6b8f6f8e
Arthur Pastel <arthur.pastel@gmail.com>
2020-10-17 16:19:05 +0200

    Test FieldProxy edge cases

--- tests/unit/test_field.py
+++ tests/unit/test_field.py
@@ -1,6 +1,8 @@
 import pytest
 
 from odmantic.field import Field
+from odmantic.model import EmbeddedModel, Model
+from odmantic.reference import Reference
 
 
 def test_field_defined_as_primary_key_and_custom_name():
@@ -21,3 +23,37 @@ def test_field_define_key_as__id_without_setting_as_primary():
         match="cannot specify key_name='_id' without defining the field as primary",
     ):
         Field(key_name="_id")
+
+
+def test_pos_key_name():
+    class M(Model):
+        field: int = Field(key_name="alternate_name")
+
+    assert +M.field == "alternate_name"
+    assert ++M.field == "$alternate_name"
+
+
+def test_unknown_attr_embedded_model():
+    class E(EmbeddedModel):
+        ...
+
+    class M(Model):
+        field: E
+
+    with pytest.raises(AttributeError, match="attribute unknown_attr not found in E"):
+        M.field.unknown_attr  # type: ignore
+
+
+@pytest.mark.parametrize("operator_name", ("lt", "lte", "gt", "gte", "match"))
+def test_reference_field_operator_not_allowed(operator_name: str):
+    class E(Model):
+        ...
+
+    class M(Model):
+        field: E = Reference()
+
+    with pytest.raises(
+        AttributeError,
+        match=f"operator {operator_name} not allowed for ODMReference fields",
+    ):
+        getattr(M.field, operator_name)
--- tests/unit/test_reference.py
+++ tests/unit/test_reference.py
@@ -15,3 +15,14 @@ def test_build_query_filter_across_reference():
         NotImplementedError, match="filtering across references is not supported"
     ):
         M.ref.a == 12
+
+
+def test_build_query_filter_across_reference_no_attribute():
+    class Referenced(Model):
+        a: int
+
+    class M(Model):
+        ref: Referenced = Reference()
+
+    with pytest.raises(AttributeError):
+        M.ref.does_not_exist  # type: ignore
