6fb7cd324162191dc63e97adea505c6b105f5ce7
art049 <art049@users.noreply.github.com>
2020-10-23 00:04:21 +0000

    deploy: 16d6e7cbef0ff2f4961ab2926a5a5021889decdc

--- examples_src/raw_query_usage/create_from_raw.py
+++ examples_src/raw_query_usage/create_from_raw.py
@@ -1,3 +1,5 @@
+from bson import ObjectId
+
 from odmantic import Field, Model
 
 
@@ -5,6 +7,8 @@ class User(Model):
     name: str = Field(key_name="username")
 
 
-user = User(name="John")
-print(user.doc())
-#> {'username': 'John', '_id': ObjectId('5f8352a87a733b8b18b0cb27')}
+document = {"username": "John", "_id": ObjectId("5f8352a87a733b8b18b0cb27")}
+
+user = User.parse_doc(document)
+print(user)
+#> id=ObjectId('5f8352a87a733b8b18b0cb27') name='John'
