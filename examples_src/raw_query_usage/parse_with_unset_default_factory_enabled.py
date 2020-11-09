eda27e6492f6292c39baf607e146ef6be6005566
art049 <art049@users.noreply.github.com>
2020-11-09 03:58:44 +0000

    deploy: fd1ddc5a5fae4a5254b7cc99864491f70f0139e2

---
+++ examples_src/raw_query_usage/parse_with_unset_default_factory_enabled.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from bson import ObjectId
+
+from odmantic import Model
+from odmantic.exceptions import DocumentParsingError
+from odmantic.field import Field
+
+
+class User(Model):
+    name: str
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+
+    class Config:
+        parse_doc_with_default_factories = True
+
+
+document = {"name": "Leeroy", "_id": ObjectId("5f8352a87a733b8b18b0cb27")}
+
+user = User.parse_doc(document)
+print(repr(user))
+#> User(
+#>     id=ObjectId("5f8352a87a733b8b18b0cb27"),
+#>     name="Leeroy",
+#>     updated_at=datetime.datetime(2020, 11, 8, 23, 28, 19, 980000),
+#> )
