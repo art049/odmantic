3cd715cf19112f657f6f357cf67619291266d3fd
Ernesto Revilla <ernesto.revilla@gmail.com>
2021-02-03 14:29:20 +0100

    fix(bson_fields): truncate microsecs inst. of round

--- odmantic/bson.py
+++ odmantic/bson.py
@@ -145,8 +145,8 @@ class _datetime(datetime):
         # https://docs.python.org/3/library/datetime.html#determining-if-an-object-is-aware-or-naive
         if d.tzinfo is not None and d.tzinfo.utcoffset(d) is not None:
             raise ValueError("datetime objects must be naive (no timeone info)")
-        # Round microseconds to the nearest millisecond to comply with Mongo behavior
-        microsecs = round(d.microsecond / 1000) * 1000
+        # Truncate microseconds to milliseconds to comply with Mongo behavior
+        microsecs = d.microsecond - d.microsecond % 1000
         return d.replace(microsecond=microsecs)
 
 
