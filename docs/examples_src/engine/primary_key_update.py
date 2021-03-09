d533596f6b151ae51af3406af3c3a9de71c61628
Arthur Pastel <arthur.pastel@gmail.com>
2021-03-09 20:20:21 +0100

    Copy and patch method documentation

--- docs/examples_src/engine/primary_key_update.py
+++ docs/examples_src/engine/primary_key_update.py
@@ -14,9 +14,9 @@ shroud = await engine.find_one(Player, Player.name == "Shroud")
 print(shroud.id)
 #> 5f86074f6dfecacc68428a62
 new_id = ObjectId("ffffffffffffffffffffffff")
-# First delete the remote instance
+# Copy the player instance with a new primary key
+new_shroud = shroud.copy(update={"id": new_id})
+# Delete the initial player instance
 await engine.delete(shroud)
-# Then, copy the player object with a new primary key
-new_shroud = Player(**{**shroud.dict(), "id": new_id})
-# Finally create again the document
+# Finally persist again the new instance
 await engine.save(new_shroud)
