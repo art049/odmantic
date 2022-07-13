41f32e3d7f429c537936590baf8cc5ce33a87829
Sebastián Ramírez <tiangolo@gmail.com>
2022-07-13 14:09:57 +0200

    🐛 Fix type annotation for Model.id

--- odmantic/model.py
+++ odmantic/model.py
@@ -751,7 +751,7 @@ class Model(_BaseODMModel, metaclass=ModelMetaclass):
         __collection__: ClassVar[str] = ""
         __primary_field__: ClassVar[str] = ""
 
-        id: Union[ObjectId, Any]  # TODO fix basic id field typing
+        id: Union[ObjectId, Any] = None  # TODO fix basic id field typing
 
     def __setattr__(self, name: str, value: Any) -> None:
         if name == self.__primary_field__:
