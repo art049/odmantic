68046799da1d85e9d5a3e2005427f90dd83de1c7
Arthur Pastel <arthur.pastel@gmail.com>
2022-08-24 22:27:39 +0200

    Remove unnecessary Python3.6 type fixes

--- odmantic/typing.py
+++ odmantic/typing.py
@@ -1,20 +1,10 @@
 import sys
 from typing import Any
+from typing import Callable as TypingCallable
 
-if sys.version_info < (3, 7):
-    from typing import Callable as Callable
-
-    NoArgAnyCallable = Callable[[], Any]
-else:
-    from collections.abc import Callable as Callable
-    from typing import Callable as TypingCallable
-
-    NoArgAnyCallable = TypingCallable[[], Any]
+NoArgAnyCallable = TypingCallable[[], Any]
 
 if sys.version_info < (3, 8):
     from typing_extensions import Literal
 else:
     from typing import Literal  # noqa: F401
-
-
-USES_OLD_TYPING_INTERFACE = sys.version_info[:3] < (3, 7, 0)  # PEP 560
