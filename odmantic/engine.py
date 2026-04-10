b117d03949572d18c6cbefad813e82c7cf6c215c
Alex Bevilacqua <alex@alexbevi.com>
2026-04-10 14:18:32 -0400

    feat: add MongoDB client metadata

--- odmantic/engine.py
+++ odmantic/engine.py
@@ -18,12 +18,15 @@ from typing import (
     cast,
 )
 
+from importlib.metadata import version as _get_version
+
 import pymongo
 from pymongo import MongoClient
 from pymongo.client_session import ClientSession
 from pymongo.collection import Collection
 from pymongo.command_cursor import CommandCursor
 from pymongo.database import Database
+from pymongo.driver_info import DriverInfo
 
 from odmantic.exceptions import DocumentNotFoundError, DuplicateKeyError
 from odmantic.field import FieldProxy, ODMReference
@@ -52,6 +55,8 @@ try:
 except ImportError:  # pragma: no cover
     motor = None
 
+
+_DRIVER_INFO = DriverInfo(name="ODMantic", version=_get_version("odmantic"))
 
 ModelType = TypeVar("ModelType", bound=Model)
 
@@ -322,7 +327,9 @@ class AIOEngine(BaseEngine):
                 + 'pip install "odmantic[motor]"'
             )
         if client is None:
-            client = AsyncIOMotorClient()
+            client = AsyncIOMotorClient(driver=_DRIVER_INFO)
+        else:
+            client.append_metadata(_DRIVER_INFO)
         super().__init__(client=client, database=database)
 
     def get_collection(self, model: Type[ModelType]) -> "AsyncIOMotorCollection":
@@ -733,7 +740,9 @@ class SyncEngine(BaseEngine):
             database: name of the database to use
         """
         if client is None:
-            client = MongoClient()
+            client = MongoClient(driver=_DRIVER_INFO)
+        else:
+            client.append_metadata(_DRIVER_INFO)
         super().__init__(client=client, database=database)
 
     def get_collection(self, model: Type[ModelType]) -> "Collection":
